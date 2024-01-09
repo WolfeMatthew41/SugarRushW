a802bf4c6d38071bcb9ffc9d2f7c2bf8c7c6ee38
Luis Serpas <serpas.icode@gmail.com>
2024-01-09 16:13:34 -0500

    Updated Cinematic Camera, Player Movement updated

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,19 +11,26 @@ public class PlayerController : MonoBehaviour
     private Vector3 _direction;
     private CharacterController _characterController;
     [SerializeField]
-    private float speed = 2f; //Used to control speed of Puff
+    private float speed = 0.05f; //Used to control speed of Puff
 
     [SerializeField]
-    private float smoothTime = 0.08f;
+    private float smoothTime = 1f;
 
     [SerializeField]
     private float _currentVelocity;
 
-    private float _gravity = -9.81f; //Used to control fall speed
+    [SerializeField]
+    private float _gravity = 9.81f; //Used to control fall speed
 
     [SerializeField] 
-    private float gravityMultiplyer = 3.0f;
+    private float gravityMultiplyer = .25f;
     private float _velocity;
+
+
+    private float rotationX;
+    private float rotationY;
+
+    private Vector3 movementDirection;
 
     private void Awake()
     {
@@ -33,28 +40,30 @@ public class PlayerController : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         _input = context.ReadValue<Vector2>();
-        Debug.Log(_input);// message: "PlayerController Moving!");
+        //Debug.Log(_input);// message: "PlayerController Moving!");
         _direction = new Vector3(_input.x, 0.0f, _input.y);
+      
+        movementDirection = _direction;
     }
 
     private void Update()
     {
-        //ApplyRotation();
-        ApplyMovement();
+        ApplyRotation();
+       
         ApplyGravity();
+
+        ApplyMovement();
     }
 
     private void ApplyRotation()
     {
-        if (_input.sqrMagnitude == 0) return; // dont rotate unless player input
+        //if (_input.x == 0) return;
+        if (_input.sqrMagnitude == 0) return;
+
         var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
         var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
         transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
-    }
 
-    private void ApplyMovement()
-    {
-        _characterController.Move(_direction * speed);
     }
 
     private void ApplyGravity()
@@ -64,5 +73,10 @@ public class PlayerController : MonoBehaviour
         else
             _velocity += _gravity * gravityMultiplyer * Time.deltaTime;
         _direction.y = _velocity;
+    }
+
+    private void ApplyMovement()
+    {
+        _characterController.Move(_direction * speed);
     }
 }
