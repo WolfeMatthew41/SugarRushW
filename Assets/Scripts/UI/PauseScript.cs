3d58ce46134a3ff288e13dca951d37ce01529bc5
STRIKENSUN41 <matthew.wolfe4001@gmail.com>
2024-01-11 13:26:19 -0600

    Added Pause Menu

---
+++ Assets/Scripts/UI/PauseScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+
+    private bool _isPaused = false;
+
+    public GameObject PauseMenu;
+
+
+
+    public void OnPause()
+    {
+        if (!_isPaused)
+        {
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            _isPaused = true;
+        }
+        else 
+        {
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            _isPaused = false;
+        }
+
+    }
+
+    public void OnSettings()
+    { 
+    }
+
+    public void OnQuit()
+    { 
+
+    }
+}
